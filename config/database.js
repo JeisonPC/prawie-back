99e9d11e7831e24a8278a974f5d889309983b758
jeison poveda <jeisonfpovedac@gmail.com>
2023-02-15 00:15:41 -0500

    fix database config

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
-module.exports = ({ env }) => ({
+//Development
+/* module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
@@ -7,6 +8,21 @@ module.exports = ({ env }) => ({
       database: env('DATABASE_NAME', 'prawie_db'),
       user: env('DATABASE_USERNAME', 'jeisonpc'),
       password: env('DATABASE_PASSWORD', 'C@shBack96'),
+      ssl: env.bool('DATABASE_SSL', false),
+    },
+  },
+}); */
+
+//Production
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('DATABASE_HOST'),
+      port: env.int('DATABASE_PORT'),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
       ssl: env.bool('DATABASE_SSL', false),
     },
   },
