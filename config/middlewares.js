464aab54f4fa95a793c4cabddafbba357d8b8aa6
jeison poveda <jeisonfpovedac@gmail.com>
2023-05-07 19:00:35 -0500

    [EDIT] Se editó un error en middleware repetido unos datos

--- config/middlewares.js
+++ config/middlewares.js
@@ -26,24 +26,3 @@ module.exports = [
   'strapi::favicon',
   'strapi::public',
 ];
-
-
-module.exports = [
-  'strapi::errors',
-  'strapi::security',
-  'strapi::poweredBy',
-  {
-    name: 'strapi::cors',
-    config: {
-      enabled: true,
-      headers: '*',
-      origin: ['http://localhost:1337', 'https://prawie.com']
-    }
-  },
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
-];
