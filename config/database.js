8994b9552b5e01748cc4f511b9792196a4303039
jeison poveda <jeisonfpovedac@gmail.com>
2023-07-04 00:00:05 -0500

    CAmbiado el port de conexión

--- config/database.js
+++ config/database.js
@@ -3,8 +3,8 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5432),
+      host: env('HOST', '127.0.0.1'),
+      port: env.int('PORT', 5432),
       database: env('DATABASE_NAME', 'prawie_db'),
       user: env('DATABASE_USERNAME', 'jeisonpc'),
       password: env('DATABASE_PASSWORD', 'C@shBack96'),
