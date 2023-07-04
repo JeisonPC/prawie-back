54ca3f9742ff93d9a36123f0c5385e94df58037c
jeison poveda <jeisonfpovedac@gmail.com>
2023-07-04 00:36:42 -0500

    Se agregó pool

--- config/database.js
+++ config/database.js
@@ -3,15 +3,20 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('HOST', '127.0.0.1'),
-      port: env.int('PORT', 5432),
+      host: env('DBHOST', '127.0.0.1'),
+      port: env.int('DBPORT', 5432),
       database: env('DATABASE_NAME', 'prawie_db'),
       user: env('DATABASE_USERNAME', 'jeisonpc'),
       password: env('DATABASE_PASSWORD', 'C@shBack96'),
       ssl: env.bool('DATABASE_SSL', false),
     },
+    pool: {
+      min: 0,
+      max: 10,
+    },
   },
 });
+
 
 //Production
 /* module.exports = ({ env }) => ({
