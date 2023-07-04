6e6d1ce0ded32e985e162f8783c6ce91e5dd2265
jeison poveda <jeisonfpovedac@gmail.com>
2023-07-04 02:07:51 -0500

    Agregado español

---
+++ src/admin/app.js
@@ -0,0 +1,19 @@
+const config = {
+  locales: [
+    'fr',
+    'cs',
+    'de',
+    'dk',
+    'es',
+    'en',
+  ],
+};
+
+const bootstrap = (app) => {
+  console.log(app);
+};
+
+export default {
+  config,
+  bootstrap,
+};
