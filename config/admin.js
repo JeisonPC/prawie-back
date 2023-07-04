c93bb91c0422b03932974beb7677539f2d836b90
jeison poveda <jeisonfpovedac@gmail.com>
2023-07-04 01:19:39 -0500

    Se agrega crypto para las variables

--- config/admin.js
+++ config/admin.js
@@ -1,8 +1,21 @@
-module.exports = ({ env }) => ({
-  apiToken: {
-    salt: env('API_TOKEN_SALT', 'd9b0df66ff97a666027e665707b4e3e7'),
-  },
-  auth: {
-    secret: env('ADMIN_JWT_SECRET', '77b2c87dbab4e1697bec244226fbd1b3'),
-  },
-});
+const crypto = require("crypto");
+
+module.exports = ({ env }) => {
+  const generateRandomToken = () => {
+    return crypto.randomBytes(16).toString("hex");
+  };
+
+  return {
+    auth: {
+      secret: env("ADMIN_JWT_SECRET") || generateRandomToken(),
+    },
+    apiToken: {
+      salt: env("API_TOKEN_SALT") || generateRandomToken(),
+    },
+    transfer: {
+      token: {
+        salt: env("TRANSFER_TOKEN_SALT") || generateRandomToken(),
+      },
+    },
+  };
+};
